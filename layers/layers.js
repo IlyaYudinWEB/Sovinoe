aac1f5fed8d33f06c9569a4a5a18c9e96aad13d6
IlyaYudinWEB <110660931+IlyaYudinWEB@users.noreply.github.com>
2024-01-15 12:49:43 +0300

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,77 +1,77 @@
 var wms_layers = [];
 
-var lyr__0 = new ol.layer.Image({
+var lyr_PseudocolorSWIR_0 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Коротковолновая инфракрасная съёмка",
+                            title: "Pseudo color SWIR",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_0.png",
+                               url: "./layers/PseudocolorSWIR_0.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
                                 imageExtent: [19833425.752589, 10852139.806594, 19892182.061129, 10895072.952053]
                             })
                         });
-var lyr__1 = new ol.layer.Image({
+var lyr_PseudocolorIR_1 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Псевдо цвета",
+                            title: "Pseudo color IR",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_1.png",
+                               url: "./layers/PseudocolorIR_1.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
                                 imageExtent: [19833425.752589, 10852139.806594, 19892182.061129, 10895072.952053]
                             })
                         });
-var lyr__2 = new ol.layer.Image({
+var lyr_Naturalcolor_2 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Естественные цвета",
+                            title: "Natural color",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_2.png",
+                               url: "./layers/Naturalcolor_2.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
                                 imageExtent: [19833425.752589, 10852139.806594, 19892182.061129, 10895072.952053]
                             })
                         });
-var lyr__3 = new ol.layer.Image({
+var lyr_Quartz1_3 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Кварцевая порода перспективный район",
+                            title: "Quartz 1",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_3.png",
+                               url: "./layers/Quartz1_3.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
                                 imageExtent: [19834054.805933, 10855954.574289, 19857500.221452, 10884830.863622]
                             })
                         });
-var lyr__4 = new ol.layer.Image({
+var lyr_Carbonate2_4 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Карбонатная порода район добычи",
+                            title: "Carbonate 2",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_4.png",
+                               url: "./layers/Carbonate2_4.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
                                 imageExtent: [19855524.875325, 10856042.244334, 19891477.674281, 10884978.454895]
                             })
                         });
-var lyr__5 = new ol.layer.Image({
+var lyr_Carbonate1_5 = new ol.layer.Image({
                             opacity: 1,
-                            title: "Карбонатная порода перспективный район",
+                            title: "Carbonate 1",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/_5.png",
+                               url: "./layers/Carbonate1_5.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
@@ -79,5 +79,5 @@ var lyr__5 = new ol.layer.Image({
                             })
                         });
 
-lyr__0.setVisible(true);lyr__1.setVisible(true);lyr__2.setVisible(true);lyr__3.setVisible(true);lyr__4.setVisible(true);lyr__5.setVisible(true);
-var layersList = [lyr__0,lyr__1,lyr__2,lyr__3,lyr__4,lyr__5];
+lyr_PseudocolorSWIR_0.setVisible(true);lyr_PseudocolorIR_1.setVisible(true);lyr_Naturalcolor_2.setVisible(true);lyr_Quartz1_3.setVisible(true);lyr_Carbonate2_4.setVisible(true);lyr_Carbonate1_5.setVisible(true);
+var layersList = [lyr_PseudocolorSWIR_0,lyr_PseudocolorIR_1,lyr_Naturalcolor_2,lyr_Quartz1_3,lyr_Carbonate2_4,lyr_Carbonate1_5];
